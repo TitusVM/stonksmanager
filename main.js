b4d99967f40943ef6be02053e08b420c5d3a5d71
Abele Titus <titusvit.abele@he-arc.ch>
2021-12-08 10:15:37 +0100

    add tabview sur gestionnaire de factures div de droite

--- main.js
+++ main.js
@@ -17,6 +17,7 @@ function createWindows() {
             nodeIntegration: true,
             contextIsolation: false,
             enableRemoteModule: true,
+            webviewTag: true
         }
     })
 
