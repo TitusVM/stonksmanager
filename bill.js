9adeb0f308ff66ce6efe93a77c226b0bc61e8a11
Moreira Miguel <miguel.moreira@he-arc.ch>
2021-12-22 10:10:16 +0100

    Left side of billsManager

--- bill.js
+++ bill.js
@@ -1,20 +1,15 @@
-const today = new Date()
-const tomorrow = new Date(today)
-tomorrow.setDate(tomorrow.getDate() + 1)
-
 class Bill {
-    constructor(category, name = "default bill", date = tomorrow.toLocaleDateString(), value = 10, monthly = false, pdfPath = "") {
+    constructor(category, name, date, value, monthly) {
       this.category = category;
       this.name = name;
       this.date = date;
       this.value = value;
       this.monthly = monthly;
-      this.pdfPath = pdfPath;
       this.paid = false;
     }
 
     logTest() {
-        console.log(this.category, this.name, this.date, this.value, this.monthly, this.pdfPath, this.paid);
+        console.log(this.category, this.name, this.date, this.value, this.monthly, this.paid);
     }
 }
 
